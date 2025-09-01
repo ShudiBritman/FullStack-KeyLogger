923dc01bdcd80f9bd8d06e776473b642d4428026
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2025-09-01 18:12:20 +0200

    An enhanced encryption key

--- FullStack-KeyLogger/KeyLogger FullStack.py
+++ FullStack-KeyLogger/KeyLogger FullStack.py
@@ -1,9 +1,12 @@
 class Encryption:
-    def __init__(self, key: int):
-        self.key = key
+    def __init__(self, key: str):
+        self.key = str(key)
+        self.key = [ord(c) for c in self.key]
     def encrypt(self, data: str):
-        return  "".join((chr(ord(c) ^ self.key)) for c in data)
+        return  "".join((chr(ord(c) ^ self.key[i % len(self.key)])) for i, c in enumerate(data))
     def decrypt(self, data: str):
         return self.encrypt(data)
+
+
 
 
