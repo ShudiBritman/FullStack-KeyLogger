b56b52301dcff8377ce654d178826d8dfbf7e5da
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2025-09-01 15:29:07 +0200

    add xor encryption script

--- FullStack-KeyLogger/KeyLogger FullStack.py
+++ FullStack-KeyLogger/KeyLogger FullStack.py
@@ -1,3 +1,14 @@
-class XorCipher()
-    def __init__(self, key: str):
-        self.key = [ord(c) for c in key]
+class Encryption:
+    def __init__(self, key: int):
+        self.key = key
+    def encrypt(self, data: str):
+        return  "".join((chr(ord(c) ^ self.key)) for c in data)
+    def decrypt(self, data: str):
+        return self.encrypt(data)
+
+my_cryption = Encryption(5)
+encryption = my_cryption.encrypt("Hello world")
+print(encryption)
+encryption = my_cryption.decrypt(encryption)
+print(encryption)
+
