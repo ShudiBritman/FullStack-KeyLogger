fb7d2e36dd96bb1b2b1ea5eb194d71e51b2c2179
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2025-09-02 13:20:09 +0200

    שינויים אחרונים לפני merge ל-main

---
+++ keylogger/encryption.py
@@ -0,0 +1,11 @@
+class Encryption:
+    def __init__(self, key="IDF"):
+        self.my_key = key
+        key = str(key)
+        self.key = [ord(c) for c in key]
+    def encrypt(self, data):
+        data = str(data)
+        return  "".join((chr(ord(c) ^ self.key[i % len(self.key)])) for i, c in enumerate(data))
+    def decrypt(self, data):
+        data = str(data)
+        return self.encrypt(data)
